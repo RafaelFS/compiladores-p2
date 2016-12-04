b463e93c4e0693b4fd4739eaeeae1a7a0fdee847
Rafael Freitas da Silva <rfl.fsilva@gmail.com>
2016-12-04 21:03:32 -0200

    Now runtimeTemplate does not execute code during compilation

--- runtimeTemplate.rb
+++ runtimeTemplate.rb
@@ -185,38 +185,9 @@ input_program = nil
 # output_generator should swap the comment below with the apropriate input_program array
 #<<MARKER_FOR_GENERATOR>>#
 
-program1 = [4, 7, 255]
-program2 = [4,32]
-program3 = [2, 32, 255, 256, 255]
-program4 = [2, 32, 256, 255]
-program5 = [258, 262, 4, 999, 255, 257]
-program6 = [Integer("0x0002"),
-Integer("0x010a"),
-Integer("0x00ff"),
-Integer("0x0032"),
-Integer("0x00ff"),
-Integer("0x010c"),
-Integer("0x010a"),
-Integer("0x0004"),
-Integer("0x010a"),
-Integer("0x0119"),
-Integer("0x0000"),
-Integer("0x00ff"),
-Integer("0x0003"),
-Integer("0x0001"),
-Integer("0x0022"),
-Integer("0x0005"),
-Integer("0x00ff"),
-Integer("0x0002"),
-Integer("0x0020"),
-Integer("0x00ff"),
-Integer("0x0003"),
-Integer("0x0022"),
-Integer("0x00ff"),
-Integer("0x0001"),
-Integer("0x00ff")]
-
-runtime = RuntimeEnvironment.new
-runtime.setup
-runtime.load(input_program || program6)
-runtime.run
+if input_program then
+  runtime = RuntimeEnvironment.new
+  runtime.setup
+  runtime.load(input_program || program6)
+  runtime.run
+end
---
+++ testPrograms.rb
@@ -0,0 +1,30 @@
+program1 = [4, 7, 255]
+program2 = [4,32]
+program3 = [2, 32, 255, 256, 255]
+program4 = [2, 32, 256, 255]
+program5 = [258, 262, 4, 999, 255, 257]
+program6 = [Integer("0x0002"),
+Integer("0x010a"),
+Integer("0x00ff"),
+Integer("0x0032"),
+Integer("0x00ff"),
+Integer("0x010c"),
+Integer("0x010a"),
+Integer("0x0004"),
+Integer("0x010a"),
+Integer("0x0119"),
+Integer("0x0000"),
+Integer("0x00ff"),
+Integer("0x0003"),
+Integer("0x0001"),
+Integer("0x0022"),
+Integer("0x0005"),
+Integer("0x00ff"),
+Integer("0x0002"),
+Integer("0x0020"),
+Integer("0x00ff"),
+Integer("0x0003"),
+Integer("0x0022"),
+Integer("0x00ff"),
+Integer("0x0001"),
+Integer("0x00ff")]
